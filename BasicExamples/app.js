8ae6de0d8fede8e2dd76c5ef0da5363409d78a9f
surya5a72 <surya5a72@gmail.com>
2020-01-23 15:40:07 +0530

    2 files modified

--- BasicExamples/app.js
+++ BasicExamples/app.js
@@ -1,4 +1,28 @@
-var template = <p> this is from react app </p>
+var template ={
+  Title: 'Indecision App',
+  Subtitle: 'Practice Session'
+}
+var user ={
+  Name:'Surya',
+  Age:28,
+  Mobile:9032586092,
+  Designation:'Full Stack Developer',
+  Location:'Rajamahendravaram'
+};
+function getlocation(Location){
+  return Location ? <p> Location : {Location} </p> : <p> Location : {'Unknow'} </p> ;
+}
+var template = (
+  <div>
+    <h1> {template.Title} </h1>
+    <h3> {template.Subtitle} </h3>
+    <p> Username :{user.Name.toUpperCase()} </p>
+    { user.Age && user.Age >= 18 && <p> Age :{user.Age} </p> }
+    <p> Mobile :{user.Mobile} </p>
+    <p> Designation :{user.Designation} </p>
+      {getlocation(user.Location)}
+  </div>
+);
 var appRoot = document.getElementById('app');
 
 render (template, appRoot);
--- index.js
+++ index.js
@@ -6,31 +6,3 @@ import './style.css';
 
 
 //render(<EmployeeDetails />, document.getElementById('root'));
-var template ={
-  Title: 'Indecision App',
-  Subtitle: 'Practice Session'
-}
-var user ={
-  Name:'Surya',
-  Age:28,
-  Mobile:9032586092,
-  Designation:'Full Stack Developer',
-  Location:'Rajamahendravaram'
-};
-function getlocation(Location){
-  return Location ? <p> Location : {Location} </p> : <p> Location : {'Unknow'} </p> ;
-}
-var template = (
-  <div>
-    <h1> {template.Title} </h1>
-    <h3> {template.Subtitle} </h3>
-    <p> Username :{user.Name.toUpperCase()} </p>
-    { user.Age && user.Age >= 18 && <p> Age :{user.Age} </p> }
-    <p> Mobile :{user.Mobile} </p>
-    <p> Designation :{user.Designation} </p>
-      {getlocation(user.Location)}
-  </div>
-);
-var appRoot = document.getElementById('app');
-
-render (template, appRoot);
