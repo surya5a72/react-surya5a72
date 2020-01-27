b2434f25d0e1c87e41ba3430812a8d3533fa529d
surya5a72 <surya5a72@gmail.com>
2020-01-27 16:26:54 +0530

    2 files modified

--- BasicExamples/app.js
+++ BasicExamples/app.js
@@ -49,3 +49,31 @@ var fullname = 'surya sairaj l';
 const getfirstnaemme = (name) => name.split(' ')[0];
 
 console.log(getfirstnaemme(fullname));
+
+const user ={
+  Name:'Surya',
+  Cities: ['Rajahmundry', 'Hyderabad', 'Bangalore'],
+  //we shouldn't use arrow function here if we use it looks for the parent scope and thus there won't be any values available for the function.
+  Workedcities:function(){
+    //console.log(this.Name);
+    //console.log(this.Cities);
+    //pure es5 function. while using function inside function the scope of "this" keyword wont be available for inside function then we need to declare a const and assign the this to anyother property then we can access the this keyword properties inside the nested function. below is example.
+    
+    // const that = this;
+    // this.Cities.forEach(function(city){
+    //   console.log(that.Name + ' is lived and worked in '+city);
+    // });
+
+    //when using arrow function inside a function this keyword will be available for the scope of arrow function below is example. 
+    // this.Cities.forEach((city)=>{
+    //   console.log(this.Name + ' is lived and worked in '+city);
+    // });
+
+    //map can be used in multiple ways below are examples
+    // return this.Cities.map((city) =>  {
+    //   return this.Name + ' is lived and worked in '+city;
+    //  });
+    return this.Cities.map((city) =>  this.Name + ' is lived and worked in '+city );
+  }
+}
+console.log(user.Workedcities());
--- index.js
+++ index.js
@@ -8,24 +8,24 @@ import './style.css';
 //render(<EmployeeDetails />, document.getElementById('root'));
 //console.log('surya');
 
-const user ={
-  Name:'Surya',
-  Cities: ['Rajahmundry', 'Hyderabad', 'Bangalore'],
-  //we shouldn't use arrow function here if we use it looks for the parent scope and thus there won't be any values available for the function.
-  Workedcities:function(){
-    console.log(this.Name);
-    console.log(this.Cities);
-    //pure es5 function. while using function inside function the scope of "this" keyword wont be available for inside function then we need to declare a const and assign the this to anyother property then we can access the this keyword properties inside the nested function. below is example.
-    
-    // const that = this;
-    // this.Cities.forEach(function(city){
-    //   console.log(that.Name + ' is lived and worked in '+city);
-    // });
 
-    //when using arrow function inside a function this keyword will be available for the scope of arrow function below is example. 
-    this.Cities.forEach((city)=>{
-      console.log(this.Name + ' is lived and worked in '+city);
-    });
-  }
+let count = 0;
+const countid='btnid';
+const add = () =>{ 
+  ++count;
+  rendertemplate();
+  console.log(count);
 }
-user.Workedcities();
+const appRoot = document.getElementById('app');
+const rendertemplate =() =>{
+  const template = (
+    <div>
+      <h1> Indecision App </h1>
+      <h3> Count : {count}</h3>
+      <button id={countid} className="btnClass" onClick={add}>+{count}</button> 
+    </div>
+  );
+  render (template, appRoot);
+};
+
+rendertemplate();
