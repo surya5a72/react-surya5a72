2ffa3e35c9265c803101dd4f67889b1c8932953a
surya5a72 <surya5a72@gmail.com>
2020-01-27 12:46:52 +0530

    2 files modified

--- BasicExamples/app.js
+++ BasicExamples/app.js
@@ -29,3 +29,23 @@ let template = (
 const appRoot = document.getElementById('app');
 
 render (template, appRoot);
+
+const square = function(x){
+  return x*x;
+}
+
+const squarearrow = (x) =>{
+  return x*x;
+}
+
+console.log(squarearrow(7));
+
+const arrsquare = (x) => x*x;
+
+console.log(arrsquare(5));
+
+var fullname = 'surya sairaj l';
+
+const getfirstnaemme = (name) => name.split(' ')[0];
+
+console.log(getfirstnaemme(fullname));
--- index.js
+++ index.js
@@ -8,22 +8,24 @@ import './style.css';
 //render(<EmployeeDetails />, document.getElementById('root'));
 //console.log('surya');
 
-const square = function(x){
-  return x*x;
+const user ={
+  Name:'Surya',
+  Cities: ['Rajahmundry', 'Hyderabad', 'Bangalore'],
+  //we shouldn't use arrow function here if we use it looks for the parent scope and thus there won't be any values available for the function.
+  Workedcities:function(){
+    console.log(this.Name);
+    console.log(this.Cities);
+    //pure es5 function. while using function inside function the scope of "this" keyword wont be available for inside function then we need to declare a const and assign the this to anyother property then we can access the this keyword properties inside the nested function. below is example.
+    
+    // const that = this;
+    // this.Cities.forEach(function(city){
+    //   console.log(that.Name + ' is lived and worked in '+city);
+    // });
+
+    //when using arrow function inside a function this keyword will be available for the scope of arrow function below is example. 
+    this.Cities.forEach((city)=>{
+      console.log(this.Name + ' is lived and worked in '+city);
+    });
+  }
 }
-
-const squarearrow = (x) =>{
-  return x*x;
-}
-
-console.log(squarearrow(7));
-
-const arrsquare = (x) => x*x;
-
-console.log(arrsquare(5));
-
-var fullname = 'surya sairaj l';
-
-const getfirstnaemme = (name) => name.split(' ')[0];
-
-console.log(getfirstnaemme(fullname));
+user.Workedcities();
